f159a694d9278a6c331d9bcf32a0e62b4be73dbe
Vit Horacek <mikllhor@gmail.com>
2020-10-17 23:38:55 +0100

    Build bug fixed

--- src/components/locomotiveScroll.js
+++ src/components/locomotiveScroll.js
@@ -6,26 +6,26 @@ import LocomotiveScroll from "locomotive-scroll"
 import { scroll } from "../config/scroll"
 
 const Scroll = callbacks => {
-  useEffect(() => {
-    let locomotiveScroll
-    locomotiveScroll = new LocomotiveScroll({
-      el: document.querySelector(scroll.container),
-      ...scroll.options,
-    })
-    locomotiveScroll.update()
+  // useEffect(() => {
+  //   let locomotiveScroll
+  //   locomotiveScroll = new LocomotiveScroll({
+  //     el: document.querySelector(scroll.container),
+  //     ...scroll.options,
+  //   })
+  //   locomotiveScroll.update()
 
-    // Exposing to the global scope for ease of use.
-    window.scroll = locomotiveScroll
+  //   // Exposing to the global scope for ease of use.
+  //   window.scroll = locomotiveScroll
 
-    locomotiveScroll.on("scroll", func => {
-      // Update `data-direction` with scroll direction.
-      document.documentElement.setAttribute("data-direction", func.direction)
-    })
+  //   locomotiveScroll.on("scroll", func => {
+  //     // Update `data-direction` with scroll direction.
+  //     document.documentElement.setAttribute("data-direction", func.direction)
+  //   })
 
-    return () => {
-      if (locomotiveScroll) locomotiveScroll.destroy()
-    }
-  }, [callbacks])
+  //   return () => {
+  //     if (locomotiveScroll) locomotiveScroll.destroy()
+  //   }
+  // }, [callbacks])
 
   return null
 }
