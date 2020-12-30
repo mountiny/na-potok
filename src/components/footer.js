d3beb1e77874393fa64ac574f0ccf79cbb7a8c98
Vit Horacek <mikllhor@gmail.com>
2020-12-30 23:18:52 +0100

    Footer social links changed for icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,29 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ChataImg from './svg/chata.js'
+import Instagram from './svg/instagram.js'
+import Facebook from './svg/facebook.js'
 
 const Footer = ({ children }) => {
 
   return (
     <footer className=''>
       <div className='container footer relative mx-auto'>
-        <div className='flex flex-wrap md:flex-row sm:flex-col justify-between relative'>
+        <div className='flex flex-wrap flex-col md-m:flex-row justify-between relative'>
           <div className='footer-text flex relative justify-start'>
             <div className='flex flex-col justify-between mr-32 md:mr-16'>
               <div className='potok'>
                 <h4 className='potok kontakt-heading'>zůstaňte v kontaktu</h4>
               </div>
-              <div className='social leading-normal pb-8'>
-                <a className='social-link underline-link mr-24' target="_blank" href='https://www.instagram.com/na_potok/'>instagram</a>
-                <a className='social-link underline-link' target="_blank" href='https://www.facebook.com/ChalupaNapotok'>facebook</a>
+              <div className='flex items-center leading-normal pb-8'>
+                <a className='social-link mr-12' target="_blank" href='https://www.facebook.com/ChalupaNapotok'>
+                  <Facebook 
+                    className='w-10 h-10' />
+                </a>
+                <a className='social-link' target="_blank" href='https://www.instagram.com/na_potok/'>
+                  <Instagram 
+                    className='w-16 h-16' />
+                </a>
               </div>
 
               <div className='address leading-normal'>
@@ -37,7 +45,7 @@ const Footer = ({ children }) => {
             width={240.006}
             height={231.879}
             viewBox="0 0 240.006 231.879"
-            className='chata-img phone:mt-16 phone:self-center' />
+            className='chata-img md:mt-16 md:self-center' />
         </div>
         
       </div>
